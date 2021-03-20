c87b95f6381216d2f3781d1eae64e13253adae42
Rasite <hrasiter@gmail.com>
2021-03-21 00:36:06 +0300

    random fortune service field injection

--- src/com/udemy/springdemo/RandomFortuneService.java
+++ src/com/udemy/springdemo/RandomFortuneService.java
@@ -1,14 +1,24 @@
 package com.udemy.springdemo;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class RandomFortuneService implements FortuneService {
 
+	private String fortunes[] = {
+			"Beware of the wolf in sheep's clothing",
+			"Diligence is the mother of good luck",
+			"The journey is the reward"
+	};
+	
+	private Random myRandom = new Random();
+	
 	@Override
 	public String getFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		int index = myRandom.nextInt(fortunes.length);
+		return fortunes[index];
 	}
 
 }
