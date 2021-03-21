40c221a048ac45c6da7ba66f2a12b85700f0778e
Rasite <hrasiter@gmail.com>
2021-03-21 15:17:36 +0300

    Bean lifecycle annotations for init and destroy @PostConstruct, @PreDestroy

--- src/com/udemy/springdemo/TennisCoach.java
+++ src/com/udemy/springdemo/TennisCoach.java
@@ -1,17 +1,30 @@
 package com.udemy.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 	
 	@Autowired
 	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
+	
+	//define init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println("TennisCoach: inside doMyStartupStuff method");
+	}
+	
+	//define destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println("TennisCoach: inside doMyCleanupStuff method");
+	}
 
 //	@Autowired
 //	public TennisCoach(@Qualifier("randomFortuneService")FortuneService fs) {
