f5adbdb3830e17f98e96bb3dce08599acd230005
Rasite <hrasiter@gmail.com>
2021-03-21 23:28:44 +0300

    reading properties file with java configuration file

--- src/com/udemy/springdemo/SwimCoach.java
+++ src/com/udemy/springdemo/SwimCoach.java
@@ -1,9 +1,23 @@
 package com.udemy.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
 
 	FortuneService fortuneService;
 	
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
+	
+	public String getEmail() {
+		return email;
+	}
+	public String getTeam() {
+		return team;
+	}
 	public SwimCoach(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
